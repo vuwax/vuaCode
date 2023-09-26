6ed1899709c3ab6e9893eb74a4a278415fcf7da1
vuwax <pinkberry.um@gmail.com>
2023-09-26 13:02:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnE1 =
+var{myname} ="mercy";
+name slice{0,3}
+var tweet=prompt{"compose your tweet"};
+var tweetcount=tweet,length;
+alert{"you have written"=tweetcount+"characters;you have"+{140=tweetcount}+"characters remaining"}
